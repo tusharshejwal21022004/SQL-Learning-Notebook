2c581f61c1a21e18d51a25a5d97a6fe403ed21ed
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-11-27 22:58:28 +0530

    Add files via upload

---
+++ Introduction.sql
@@ -0,0 +1,13 @@
+
+-- sys_configQuery : A Command to communicate with database to create manipulate and retrieve data
+-- SQL :A Structured Query Language a query use to query relational Databases.
+-- MYSQL : Data Base Management System
+
+-- Types of Command
+/* 
+1 - DDL : Data Defination Language (Create Alter Drop)
+2 - DML : Data Manipulate Language (Insert update Delete)
+3 - DQL : Data Query Language (select)
+4 - TCL : Transaction Control Language (Commit ROll)
+5 - DCL : Data Control Language (Grant Revoke)
+*/ 
